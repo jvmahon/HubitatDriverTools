7923a6429a7373e705ec0000251f06ef1b6b358a
jvmahon <jvm33@columbia.edu>
2021-06-30 06:04:42 -0400

    Add files via upload

---
+++ sensorTools.groovy
@@ -0,0 +1,9 @@
+library (
+        base: "driver",
+        author: "bcopeland",
+        category: "zwave",
+        description: "Motion",
+        name: "sensorTools",
+        namespace: "sensorTools",
+        documentationLink: ""
+)
